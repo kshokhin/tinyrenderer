c039bfc5db07d6e09ab0ef3d66be3f70de1dfc0f
Kirill Shokhin <koshokhin@gmail.com>
2019-01-12 12:06:49 +0300

    Renderer refactoring.

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -27,15 +27,10 @@ void sk::renderer::draw_line(int x0,int y0,int x1,int y1, const TGAColor& c)
 
 void sk::renderer::draw(const sk::line& l, const TGAColor& c)
 {
-    draw_line_impl(l, true, c);
-}
-
-void sk::renderer::draw_line_impl(const sk::line& l, bool steep, const TGAColor& c)
-{
     auto point = l.start();
     auto err = 0;
 
-    while(point.x != l.end().x)
+    while (point.x != l.end().x)
     {
         l.steep() ? m_image.set(point.y, point.x, c) : m_image.set(point.x, point.y, c);
 
@@ -43,43 +38,40 @@ void sk::renderer::draw_line_impl(const sk::line& l, bool steep, const TGAColor&
     }
 }
 
-void sk::renderer::draw(const model& m)
+void sk::renderer::draw_wireframe(const model& m, const TGAColor& c)
 {
     for (const auto& f : m.faces)
     {
     const auto& v0 = m.verts[f[0]];
     const auto& v1 = m.verts[f[1]];
     const auto& v2 = m.verts[f[2]];
-        draw_triangle(v0, v1, v2);
+        draw_triangle(v0, v1, v2, c);
     }
 }
 
 void sk::renderer::draw_triangle(
         const sk::vec3f& v0,
         const sk::vec3f& v1,
-        const sk::vec3f& v2)
+        const sk::vec3f& v2,
+        const TGAColor& c)
 {
-    draw_line(v0, v1);
-    draw_line(v1, v2);
-    draw_line(v2, v0);
+    draw_line(v0, v1, c);
+    draw_line(v1, v2, c);
+    draw_line(v2, v0, c);
 }
 
-void sk::renderer::draw_line(const sk::vec3f& v0, const sk::vec3f& v1)
+void sk::renderer::draw_line(const sk::vec3f& v0, const sk::vec3f& v1, const TGAColor& c)
 {
-    TGAColor red(255, 0, 0, 255);
-    int x0 = (v0[0] + 1.) * m_image.get_width() / 2.;
-    int y0 = (v0[1] + 1.) * m_image.get_height() / 2.;
-    int x1 = (v1[0] + 1.) * m_image.get_width() / 2.;
-    int y1 = (v1[1] + 1.) * m_image.get_height() / 2.;
-    draw_line(x0, y0, x1, y1, red); 
+    auto image_v0 = from_ndc(v0), image_v1 = from_ndc(v1);
+    draw_line(image_v0[0], image_v0[1], image_v1[0], image_v1[1], c);
 }
 
 
 void sk::renderer::draw_filled_triangle(
-        const sk::vec3f& v0,
-        const sk::vec3f& v1,
-        const sk::vec3f& v2,
-        const TGAColor& c)
+    const sk::vec3f& v0,
+    const sk::vec3f& v1,
+    const sk::vec3f& v2,
+    const TGAColor& c)
 {
     if (v0[1] == v1[1] && v0[1] == v2[1] || v0[0] == v1[0] && v0[0] == v2[0]) return;
 
@@ -92,24 +84,6 @@ void sk::renderer::draw_filled_triangle(
         if (!t.contains(p)) continue;
 
         m_image.set(p.x, p.y, c);
-    }
-}
-
-void sk::renderer::sort_left_to_right(sk::vec3f& v0, sk::vec3f& v1, sk::vec3f& v2)
-{
-    if (v0[0] > v1[0])
-    {
-        std::swap(v0, v1);
-    }
-
-    if (v0[0] > v2[0])
-    {
-        std::swap(v0, v2);
-    }
-    
-    if (v1[0] > v2[0])
-    {
-        std::swap(v1, v2);
     }
 }
 
@@ -137,21 +111,4 @@ sk::point sk::renderer::get_next_line_point(const sk::line& l, const point& prev
     }
 
     return sk::point(x,y);
-}
-
-sk::point sk::renderer::get_prev_line_point(const sk::line& l, const point& prev_point, int& err)
-{
-    if (prev_point.x == l.start().x) return prev_point;
-
-    auto x = prev_point.x - 1;
-    auto y = prev_point.y;
-
-    err += l.derr();
-    if (err >= l.dx())
-    {
-        y -= l.dy() > 0 ? 1 : -1;
-        err -= 2 * l.dx();
-    }
-
-    return sk::point(x, y);
 }
--- src/renderer.h
+++ src/renderer.h
@@ -26,16 +26,13 @@ public:
      
     void draw(const sk::point&, const TGAColor&);
     void draw(const sk::line&, const TGAColor&);
-    void draw(const model&);
+    void draw_wireframe(const model&, const TGAColor&);
     void draw_line(int /*x0*/,int /*y0*/,int /*x1*/,int /*y1*/, const TGAColor&);
-    void draw_line(const sk::vec3f& /*v0*/, const sk::vec3f& /*v1*/);
-    void draw_triangle(const sk::vec3f& /*v0*/, const sk::vec3f& /*v1*/, const sk::vec3f& /*v2*/);
+    void draw_line(const sk::vec3f& /*v0*/, const sk::vec3f& /*v1*/, const TGAColor&);
+    void draw_triangle(const sk::vec3f& /*v0*/, const sk::vec3f& /*v1*/, const sk::vec3f& /*v2*/, const TGAColor&);
     void draw_filled_triangle(const sk::vec3f& /*v0*/, const sk::vec3f& /*v1*/, const sk::vec3f& /*v2*/, const TGAColor&); 
 private:
-    void draw_line_impl(const sk::line&, bool /*steep*/, const TGAColor&);
     sk::point get_next_line_point(const sk::line&, const point& /*prev_point*/, int& /*err*/);
-    sk::point get_prev_line_point(const sk::line&, const point& /*prev_point*/, int& /*err*/);
-    void sort_left_to_right(sk::vec3f& /*v0*/, sk::vec3f& /*v1*/, sk::vec3f& /*v2*/);
     sk::vec3i from_ndc(const sk::vec3f& /*v*/);
 private:
     TGAImage& m_image;
